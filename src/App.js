71b45d0c93d922d076a6d873ff8c9077858f6d0d
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-13 10:59:44 +0330

    start writing Navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './componen
 const App = () => {
   return (
     <BrowserRouter>
-        <Box>
+        <Box sx={{ backgroundColor: '#000' }}>
           <Navbar/>
           <Routes>
             <Route path='/' exact element={<Feed/>}/>
