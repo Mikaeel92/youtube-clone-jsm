a4a6fada9ee9afb43ccb870cbd27020087f3e297
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2025-01-15 12:26:54 +0330

    showing content on channelDetail
    first bg color,
    we used https://cssgradient.io/

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -6,8 +6,28 @@ import { Videos, ChannelCard } from "./";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
+    const [channelDetail, setChannelDetail] = useState(null)
+    const [videos, setVideos] = useState(null);
+    const {id} = useParams()
+
+    useEffect(() => {
+        fetchFromAPI(`channels?part=snippet&id=${id}`)
+        .then((data) => setChannelDetail(data?.items[0]))
+
+        fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`)
+        .then((data) => setVideos(data?.items))
+    }, [id])
+
   return (
-    <div>ChannelDetail</div>
+    <Box minHeight="95vh">
+    <Box>
+      <div style={{
+        height:'300px',
+        background: 'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+        zIndex: 10,
+      }} />
+    </Box>
+    </Box>
   )
 }
 
